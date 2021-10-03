7822e921a2654bee5ab8423bb6783027ce6c820a
ifjso <ojsboy@hotmail.com>
2021-10-03 20:08:22 +0900

    feat: add my first spring batch job hello world

--- hello-world/src/main/java/io/spring/batch/helloworld/HelloWorldApplication.java
+++ hello-world/src/main/java/io/spring/batch/helloworld/HelloWorldApplication.java
@@ -1,13 +1,45 @@
 package io.spring.batch.helloworld;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+@EnableBatchProcessing
 @SpringBootApplication
 public class HelloWorldApplication {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    public HelloWorldApplication(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    @Bean
+    public Step step() {
+        return this.stepBuilderFactory.get("step1")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("Hello, World!");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Job job() {
+        return this.jobBuilderFactory.get("job")
+                .start(step())
+                .build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
     }
-
 }
